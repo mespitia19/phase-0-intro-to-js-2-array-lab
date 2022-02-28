88639ca1e8672d5ddf9ad93a8a301a97855d3978
MaximoEspitia <max19espitia@gmail.com>
2022-02-28 00:41:13 -0600

    commit tree f333c8fb6481147d0ade20569a773cb9a3a1713e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(2)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(0)
+}
+
+function appendCat(name_) {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = ["Arnold", ...cats]
+    return newCats;
+}
+
+function removeLastCat(name) {
+    return cats.slice(0, cats.length -1)
+}
+
+function removeFirstCat(name) {
+    const copyCats = cats.slice(1);
+    return copyCats;
+}
